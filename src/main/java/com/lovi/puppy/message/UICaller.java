3c7803d09478152b104e8766709efe45e8e6924e
Tharanga <tharanganilupul@gmail.com>
2016-04-23 15:12:47 +0530

    modified UICaller

--- src/main/java/com/lovi/puppy/message/UICaller.java
+++ src/main/java/com/lovi/puppy/message/UICaller.java
@@ -5,7 +5,7 @@ import com.lovi.puppy.exceptions.UICallerException;
 public interface UICaller {
 	
 	/**
-	 * listener address format -> {appName}.{address}
+	 * listener address format -> {appName}.{address}.
 	 * @param address
 	 * @param message
 	 * @throws ServiceCallerException
